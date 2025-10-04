faf98db54389066db720ab3867ad3f13e5624bbf
Anders G. Jørgensen <spirit55555@gmail.com>
2025-10-04 12:55:56 +0200

    Add better error handling to Thereads

--- threads.go
+++ threads.go
@@ -9,10 +9,14 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 type ThreadsResponse struct {
-	Id string
+	Id    string
+	Error struct {
+		Message string
+	}
 }
 
 func postToThreads(post, reply, link string) {
@@ -25,19 +29,45 @@ func postToThreads(post, reply, link string) {
 
 	//Create post
 	collection := makeRequestToThreads("threads", url.Values{"media_type": {"TEXT"}, "text": {post}})
-	log.Printf("Threads collection ID: %s", collection.Id)
+
+	if collection.Id != "" {
+		log.Printf("Threads collection ID: %s", collection.Id)
+	} else {
+		log.Printf("Threads collection error: %s", collection.Error.Message)
+	}
+
+	//Sleep for 5 secs, collection should be ready by then
+	time.Sleep(5 * time.Second)
 
 	//Publish post
 	publish := makeRequestToThreads("threads_publish", url.Values{"creation_id": {collection.Id}})
-	log.Printf("Threads publish ID: %s", publish.Id)
+
+	if publish.Id != "" {
+		log.Printf("Threads publish ID: %s", publish.Id)
+	} else {
+		log.Printf("Threads public error: %s", publish.Error.Message)
+	}
 
 	//Create reply
 	replyCollection := makeRequestToThreads("threads", url.Values{"media_type": {"TEXT"}, "text": {reply}, "reply_to_id": {publish.Id}})
-	log.Printf("Threads reply collection ID: %s", replyCollection.Id)
+
+	if replyCollection.Id != "" {
+		log.Printf("Threads reply collection ID: %s", replyCollection.Id)
+	} else {
+		log.Printf("Threads reply collection error: %s", replyCollection.Error.Message)
+	}
+
+	//Sleep for 5 secs, reply should be ready by then
+	time.Sleep(5 * time.Second)
 
 	//Post reply
 	replyPublish := makeRequestToThreads("threads_publish", url.Values{"creation_id": {replyCollection.Id}})
-	log.Printf("Threads reply publish ID: %s", replyPublish.Id)
+
+	if replyPublish.Id != "" {
+		log.Printf("Threads reply publish ID: %s", replyPublish.Id)
+	} else {
+		log.Printf("Threads reply publish error: %s", replyPublish.Error.Message)
+	}
 }
 
 func makeRequestToThreads(endpoint string, params url.Values) (response ThreadsResponse) {
@@ -51,6 +81,12 @@ func makeRequestToThreads(endpoint string, params url.Values) (response ThreadsR
 	}
 
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("Threads returned \"%s\" for request to %s", resp.Status, resp.Request.URL)
+
+		return response
+	}
 
 	respBody, err := io.ReadAll(resp.Body)
 
