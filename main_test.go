9682931d9348c946f0a189c60ee715c647c7bb50
Anders G. Jørgensen <spirit55555@gmail.com>
2026-05-17 20:26:34 +0200

    Test if albums are sorted in ascending order

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"os"
+	"slices"
 	"testing"
 )
 
@@ -11,5 +12,14 @@ func TestIfCoverImagesExist(t *testing.T) {
 		if _, err := os.Stat("images/" + albumName + ".png"); errors.Is(err, os.ErrNotExist) {
 			t.Errorf("Cover image not found for album: %s", albumName)
 		}
+	}
+}
+
+func TestAlbumsIsSorted(t *testing.T) {
+	if !slices.IsSorted(albums) {
+		sortedALbums := slices.Clone(albums)
+		slices.Sort(sortedALbums)
+
+		t.Errorf("Albums are not sorted in ascending order.\nFound:    %v\nExpected: %v", albums, sortedALbums)
 	}
 }
